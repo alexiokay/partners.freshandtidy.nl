05e7d2bafcb8906e23d6d44295ac410271432f29
alexispace <papadi166@gmail.com>
2023-07-26 20:41:08 +0200

    little menu changes

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -5,7 +5,18 @@
 div#content(class="text-black w-full h-full px-6 py-[0.4rem] flex flex-col justify-start items-start gap-y-6")
     
     div(class='w-full h-auto')
-      p(class="text-2xl font-semibold") Overview
+      div(class="w-full flex items-center gap-x-3")
+        p(class="text-2xl font-semibold ") Overview
+        p(class="ml-8") Yearly
+        Switch(
+          v-model="yearlyMode"
+          :class="yearlyMode ? 'bg-blue-600' : 'bg-gray-200'" 
+          class="relative inline-flex h-6 w-11 items-center rounded-full")
+          span(class="sr-only") Enable notifications
+          span(
+            :class="yearlyMode ? 'translate-x-6' : 'translate-x-1'"
+            class="inline-block h-4 w-4 transform rounded-full bg-white transition")
+        p Monthly
     
       div(class="h-[7rem] w-full flex  mt-2 justify-between")
         DashboardQuickMenuItem(text="Calls" :value="todayShipments")
@@ -32,9 +43,13 @@ div#content(class="text-black w-full h-full px-6 py-[0.4rem] flex flex-col justi
 import PackageIcon from "~icons/ph/package-duotone";
 import { useUserStore } from "@/stores/User";
 import { useMainStore } from "@/stores/Main";
+import { Switch } from "@headlessui/vue";
+
 const mainStore = useMainStore();
 const userStore = useUserStore();
 const route = useRoute();
+
+const yearlyMode = ref(true);
 // definePageMeta({
 //   middleware: ["redirect-if-not-logged"],
 // });
