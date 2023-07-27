9bcc8c7f1973c2d9eb69bb54e7f3cfd7794d3485
alexispace <papadi166@gmail.com>
2023-07-27 21:21:46 +0200

    .

--- components/Service.vue
+++ components/Service.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
 div(@click="$emit('select', title)" :class="bgColor" :id="title.replace(/ /g, '-')" class="relative flex h-auto hover:animate-[bump_0.3s_ease-in-out]  flex-col lg:w-full w-full gap-y-4 md:gap-y-6 text-[#02374b] lg:text-center items-center lg:items-center shadow-[0_1px_60px_-15px_rgba(0,0,0,0.1)] rounded-3xl p-[2rem] md:px-[4rem] hover:cursor-pointer")
+    nuxt-img(:src="image? image: 'icons/icon-13.png'" width="120" h="45" class="w-[52%] md:w-auto h-auto  md:h-auto ")
     h3(class="font-bold text-2xl md:text-3xl") {{ title }}
     p(class="text-xl md:text-xl font-medium text-[#7f7d7e]") {{ desc }}
     div.set-up(class=" absolute top-5 right-5") set up
