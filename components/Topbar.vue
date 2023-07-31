c975332f0160f72fbd0ed1265d7c0ff01fa05f4c
alexispace <papadi166@gmail.com>
2023-07-31 03:07:54 +0200

    .

--- components/Topbar.vue
+++ components/Topbar.vue
@@ -1,11 +1,15 @@
 <template lang="pug">
-div(class="h-[var(--topbar-height)] w-full bg-white shadow-lg px-8")
-
-  UserInfo( @mouseenter="necessaryStore.setUserDropdown(true)" @mouseleave="necessaryStore.setUserDropdown(false)" class="w-auto ml-auto")
+div(class="h-[var(--topbar-height)] w-full bg-white shadow-lg px-8 items-center justify-end flex gap-x-8")
+  NotificationIcon(class="w-7 h-7")
+  MailIcon(class="w-7 h-7 ")
+  UserInfo( @mouseenter="necessaryStore.setUserDropdown(true)" @mouseleave="necessaryStore.setUserDropdown(false)" class="w-auto ")
   UserDropdown(v-if="necessaryStore.isUserDropdown" @mouseenter="necessaryStore.setUserDropdown(true)" @mouseleave="necessaryStore.setUserDropdown(false)" class="w-auto mr-auto fixed right-10 top-[4.5rem] ")
 </template>
 
 <script setup lang="ts">
+import MailIcon from "~icons/octicon/mail-24";
+import NotificationIcon from "~icons/clarity/notification-line";
+
 import { useNecessaryStore } from "@/stores/Necessary";
 const necessaryStore = useNecessaryStore();
 </script>
