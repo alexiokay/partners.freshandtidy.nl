74af06b4294fb449bcd1319421ac0a5b8ee57e16
alexispace <papadi166@gmail.com>
2023-08-01 03:00:31 +0200

    fixed nuxt image errors

--- components/Service.vue
+++ components/Service.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 div(@click="$emit('select', title)" :class="bgColor" :id="title.replace(/ /g, '-')" class="relative flex h-auto hover:animate-[bump_0.3s_ease-in-out]  flex-col lg:w-[calc(25%-1rem)] w-full gap-y-4 md:gap-y-6 text-[#02374b] lg:text-center items-center lg:items-center shadow-[0_1px_60px_-15px_rgba(0,0,0,0.1)] rounded-3xl p-[2rem] md:px-[4rem] hover:cursor-pointer")
-    nuxt-img(:src="image? image: 'icons/icon-13.png'" width="190" h="45" class="w-[52%] md:w-auto h-auto  md:h-auto ")
+    slot(name="icon")
+   
     h3(class="font-bold text-2xl md:text-3xl") {{ title.split(" ")[0] }}
       p() {{ title.split(" ")[1] }}
 
@@ -17,10 +18,6 @@ const props = defineProps({
     required: false,
   },
 
-  image: {
-    type: String,
-    required: false,
-  },
   selected: {
     type: String,
     required: false,
