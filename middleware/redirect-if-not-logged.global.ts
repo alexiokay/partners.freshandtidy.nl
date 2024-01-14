3547ed55cd74d0874af9b66e8320b528944d0a12
alexispace <papadi166@gmail.com>
2024-01-14 01:52:53 +0100

    .

--- middleware/redirect-if-not-logged.global.ts
+++ middleware/redirect-if-not-logged.global.ts
@@ -5,4 +5,12 @@ export default defineNuxtRouteMiddleware((from, to) => {
   if (userStore.getIsLogged) {
     if (from.path === "/login") return "/";
   }
+
+  if (!userStore.getIsLogged) {
+    if (from.path === "/login") return;
+    if (from.path === "/register") return;
+    // if (from.path === "/forgot-password") return;
+    if (from.path === "/waiting-list") return;
+    return "/waiting-list";
+  }
 });
