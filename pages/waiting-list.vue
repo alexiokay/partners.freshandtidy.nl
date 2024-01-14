5fcb7ed278297d3bda508544c13d68a2c2908ac5
alexispace <papadi166@gmail.com>
2024-01-14 22:39:08 +0100

    waiting list styling

--- pages/waiting-list.vue
+++ pages/waiting-list.vue
@@ -85,29 +85,30 @@ div(class="w-[100vw] h-full lg:-ml-[23rem] -mt-[4rem] flex flex-col font-intern 
                 //- nuxt-img(src="images/v2/star.png" width="60px" class=" absolute -top-[2rem] -right-[2rem]")
                 //- nuxt-img(src="images/v2/star1.png" width="50px" class=" absolute top-[0rem] -right-[6rem]")
                 //- nuxt-img(src="images/v2/star.png" width="40px" class=" absolute top-[2rem] -left-[3rem]")
-                p(class="w-auto flex flex-wrap gap-x-3 items-center justify-center") Check out our
+                p(class="w-auto flex flex-wrap gap-x-3 items-center justify-center text-3xl lg:text-4xl") Check out our
                     span(class="text-[#7863FA]") Demo
-                p(class=" w-auto text-lg font-normal hidden md:flex mt-3 ") (Client Side)
+                p(class=" w-auto text-lg font-normal  mt-1 lg:mt-3 ") (Client Side)
                    
 
-            nuxt-img(src="images/demo/freshandtidy.png" format="webp" class="w-3/5 h-full  border-[1px] border-black" )
+            nuxt-img(src="images/demo/freshandtidy.png" format="webp" class="w-[93%] lg:w-3/5 h-full  border-[1px] border-[#000000a1]" )
 
-            div(class="flex flex-col w-full items-center justify-center ")
+            div(class="flex flex-col w-[93%] items-center justify-center px-2 lg:px-0")
                 div(class="flex mt-[2rem]  w-auto relative flex-col text-3xl lg:text-[3rem] items-center  font-bold")
                     //- nuxt-img(src="images/v2/star.png" width="60px" class=" absolute -top-[2rem] -right-[2rem]")
                     //- nuxt-img(src="images/v2/star1.png" width="50px" class=" absolute top-[0rem] -right-[6rem]")
                     //- nuxt-img(src="images/v2/star.png" width="40px" class=" absolute top-[2rem] -left-[3rem]")
-                    p(class="w-auto flex flex-wrap gap-x-3 items-center justify-center") The 
+                    p(class="w-auto flex flex-wrap gap-x-3 items-center justify-center text-3xl lg:text-[3rem]") The 
                         span(class="text-[#7863FA]") booking
                         span(class="")  process
+                    p(class=" w-auto text-lg font-normal  mt-1 lg:mt-3 text-center") (Every service includes diffrent questions)
                     
                     
-                div(class="flex gap-x-8 w-3/4")
-                    nuxt-img(src="images/demo/FTP1.png" format="webp" class="w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1] rounded-3xl" )
-                    nuxt-img(src="images/demo/FTP2.png" format="webp" class="w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  rounded-3xl" )
-                div(class="flex gap-x-8 w-3/4 -mt-3")
-                    nuxt-img(src="images/demo/FTP3.png" format="webp" class="w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  rounded-3xl" )
-                    nuxt-img(src="images/demo/FTP4.png" format="webp" class="w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  rounded-3xl" )
+                div(class="flex-col md:flex gap-x-8 w-full lg:w-3/4 ")
+                    nuxt-img(src="images/demo/FTP1.png" format="webp" class="w-full lg:w-1/2 h-full mt-8 lg:mt-[3rem] border-[1px] border-[#000000a1] lg:rounded-3xl" )
+                    nuxt-img(src="images/demo/FTP2.png" format="webp" class="w-full lg:w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+                div(class="flex-col md:flex gap-x-8  -mt-3 w-full lg:w-3/4")
+                    nuxt-img(src="images/demo/FTP3.png" format="webp" class="w-full lg:w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+                    nuxt-img(src="images/demo/FTP4.png" format="webp" class="w-full lg:w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
 
 
 </template>
