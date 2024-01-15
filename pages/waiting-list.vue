287aa73bf1c4a1d39341d9128b21ef0eac68e1d5
alexispace <papadi166@gmail.com>
2024-01-15 03:12:39 +0100

    waiting list improvements

--- pages/waiting-list.vue
+++ pages/waiting-list.vue
@@ -81,19 +81,19 @@ div(class="w-[100vw] h-full lg:-ml-[23rem] -mt-[4rem] flex flex-col font-intern 
 
                     p  Get started
 
-    
-
+    //- partners demo
     div(class="flex flex-col w-full mt-[2rem] gap-y-12 items-center text-black  pt-[2rem] pb-[2rem] lg:pb-[9rem] bg-white")
             div(class="flex  w-auto relative flex-col text-3xl lg:text-[3rem] items-center  font-bold")
                 //- nuxt-img(src="images/v2/star.png" width="60px" class=" absolute -top-[2rem] -right-[2rem]")
                 //- nuxt-img(src="images/v2/star1.png" width="50px" class=" absolute top-[0rem] -right-[6rem]")
                 //- nuxt-img(src="images/v2/star.png" width="40px" class=" absolute top-[2rem] -left-[3rem]")
                 p(class="w-auto flex flex-wrap gap-x-3 items-center justify-center text-3xl lg:text-[3rem]") Check out our
-                    span(class="text-[#7863FA]") Demo
-                p(class=" w-auto text-lg font-normal  mt-1 lg:mt-3 ") (Client Side)
+                    span(class="text-[#7863FA]") MVP
+                p(class=" w-auto text-lg font-normal  mt-1 lg:mt-3 ") (Contractor side, Some screenshoots of our Minimum Viable Product)
                    
 
-            nuxt-img(src="images/demo/freshandtidy.png" format="webp" class="w-[93%] lg:w-3/5 h-full  border-[1px] border-[#000000a1]" )
+            nuxt-img(src="images/demo/partners/services.png" format="webp" class="w-[93%] lg:w-3/5 h-full  border-[1px] border-[#000000a1]" )
+            p(class=" w-auto text-lg font-normal -mt-9  text-center") (Set your services, prices, etc)
 
             div(class="flex flex-col w-[93%] items-center justify-center px-2 lg:px-0")
                 div(class="flex mt-[2rem] lg:mt-[4rem]  w-auto relative flex-col text-3xl lg:text-[3rem] items-center  font-bold")
@@ -101,18 +101,22 @@ div(class="w-[100vw] h-full lg:-ml-[23rem] -mt-[4rem] flex flex-col font-intern 
                     //- nuxt-img(src="images/v2/star1.png" width="50px" class=" absolute top-[0rem] -right-[6rem]")
                     //- nuxt-img(src="images/v2/star.png" width="40px" class=" absolute top-[2rem] -left-[3rem]")
                     p(class="w-auto flex flex-wrap gap-x-3 items-center justify-center text-3xl lg:text-[3rem]") The 
-                        span(class="text-[#7863FA]") booking
+                        span(class="text-[#7863FA]") onboarding
                         span(class="")  process
-                    p(class=" w-auto text-lg font-normal  mt-1 lg:mt-3 text-center") (Every service includes diffrent questions)
+                    p(class=" w-auto text-lg font-normal  mt-1 lg:mt-3 text-center") (Some screenshoots of our Minimum Viable Product)
                     
                     
-                div(class="flex-col md:flex-row flex gap-x-8 w-full lg:w-3/5 ")
-                    nuxt-img(src="images/demo/FTP1X.png" format="webp" class="w-full lg:w-1/2 h-full mt-8 lg:mt-[3rem] border-[1px] border-[#000000a1] lg:rounded-3xl" )
-                    nuxt-img(src="images/demo/FTP2X.png" format="webp" class="w-full lg:w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
-                div(class="flex-col md:flex-row flex gap-x-8 w-full lg:w-3/5 ")
-                    nuxt-img(src="images/demo/FTP3X.png" format="webp" class="w-full lg:w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
-                    nuxt-img(src="images/demo/FTP4X.png" format="webp" class="w-full lg:w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
-
+                div(class="flex-col flex gap-x-8 w-full lg:w-4/5 ")
+                    nuxt-img(src="images/demo/partners/localizations1.png" format="webp" class="w-full  h-full mt-8 lg:mt-[3rem] border-[1px] border-[#000000a1] lg:rounded-3xl" )
+                    p(class=" w-auto text-lg font-normal  text-center") (Choose your working area)
+                    nuxt-img(src="images/demo/partners/quotes.png" format="webp" class="w-full  h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+                    p(class=" w-auto text-lg font-normal  text-center") (Review and manage quotes)
+                //- div(class="flex-col flex gap-x-8 w-full lg:w-4/5 ")
+                //-     nuxt-img(src="images/demo/FTP3X.png" format="webp" class="w-full h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+                //-     nuxt-img(src="images/demo/FTP4X.png" format="webp" class="w-full  h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+    
+    
+   
     div(class="flex flex-col w-full mt-[2rem] gap-y-12 lg:gap-y-24 items-center text-black  pt-[2rem] pb-[6rem] bg-white")
             div(class="flex  w-auto relative flex-col text-3xl lg:text-[3rem] items-center  font-bold")
                 //- nuxt-img(src="images/v2/star.png" width="60px" class=" absolute -top-[2rem] -right-[2rem]")
@@ -168,7 +172,37 @@ div(class="w-[100vw] h-full lg:-ml-[23rem] -mt-[4rem] flex flex-col font-intern 
                         div(class="flex flex-col items-start justify-start ")
                                 p(class="font-bold text-start") Expand Your Reach: 
                                     span(class="font-normal") Grow your client base and scale your business, leveraging the insights and tools from FreshAndTidy.
-                        
+    div(class="flex flex-col w-full mt-[2rem] gap-y-12 items-center text-black  pt-[2rem] pb-[2rem] lg:pb-[9rem] bg-white")
+            div(class="flex  w-auto relative flex-col text-3xl lg:text-[3rem] items-center  font-bold")
+                //- nuxt-img(src="images/v2/star.png" width="60px" class=" absolute -top-[2rem] -right-[2rem]")
+                //- nuxt-img(src="images/v2/star1.png" width="50px" class=" absolute top-[0rem] -right-[6rem]")
+                //- nuxt-img(src="images/v2/star.png" width="40px" class=" absolute top-[2rem] -left-[3rem]")
+                p(class="w-auto flex flex-wrap gap-x-3 items-center justify-center text-3xl lg:text-[3rem]") Check out our
+                    span(class="text-[#7863FA]") Client Side 
+                    span Portal
+                p(class=" w-auto text-lg font-normal  mt-1 lg:mt-3 ")
+                   
+
+            nuxt-img(src="images/demo/freshandtidy.png" format="webp" class="w-[93%] lg:w-3/5 h-full  border-[1px] border-[#000000a1]" )
+
+            div(class="flex flex-col w-[93%] items-center justify-center px-2 lg:px-0")
+                div(class="flex mt-[2rem] lg:mt-[4rem]  w-auto relative flex-col text-3xl lg:text-[3rem] items-center  font-bold")
+                    //- nuxt-img(src="images/v2/star.png" width="60px" class=" absolute -top-[2rem] -right-[2rem]")
+                    //- nuxt-img(src="images/v2/star1.png" width="50px" class=" absolute top-[0rem] -right-[6rem]")
+                    //- nuxt-img(src="images/v2/star.png" width="40px" class=" absolute top-[2rem] -left-[3rem]")
+                    p(class="w-auto flex flex-wrap gap-x-3 items-center justify-center text-3xl lg:text-[3rem]") The 
+                        span(class="text-[#7863FA]") booking
+                        span(class="")  process
+                    p(class=" w-auto text-lg font-normal  mt-1 lg:mt-3 text-center") (Every service includes diffrent questions)
+                    
+                    
+                div(class="flex-col md:flex-row flex gap-x-8 w-full lg:w-3/5 ")
+                    nuxt-img(src="images/demo/FTP1X.png" format="webp" class="w-full lg:w-1/2 h-full mt-8 lg:mt-[3rem] border-[1px] border-[#000000a1] lg:rounded-3xl" )
+                    nuxt-img(src="images/demo/FTP2X.png" format="webp" class="w-full lg:w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+                div(class="flex-col md:flex-row flex gap-x-8 w-full lg:w-3/5 ")
+                    nuxt-img(src="images/demo/FTP3X.png" format="webp" class="w-full lg:w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+                    nuxt-img(src="images/demo/FTP4X.png" format="webp" class="w-full lg:w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+                    
 </template>
 
 <script setup lang="ts">
