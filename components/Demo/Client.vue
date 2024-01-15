2106fda55d5bd8acaef4a04e7f92e5fed7e14d93
alexispace <papadi166@gmail.com>
2024-01-15 03:17:55 +0100

    waiting list improvements

---
+++ components/Demo/Client.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+div(class="flex flex-col w-full mt-[2rem] gap-y-12 items-center text-black  pt-[2rem] pb-[2rem] lg:pb-[9rem] bg-white")
+    div(class="flex  w-auto relative flex-col text-3xl lg:text-[3rem] items-center  font-bold")
+        //- nuxt-img(src="images/v2/star.png" width="60px" class=" absolute -top-[2rem] -right-[2rem]")
+        //- nuxt-img(src="images/v2/star1.png" width="50px" class=" absolute top-[0rem] -right-[6rem]")
+        //- nuxt-img(src="images/v2/star.png" width="40px" class=" absolute top-[2rem] -left-[3rem]")
+        p(class="w-auto flex flex-wrap gap-x-3 items-center justify-center text-3xl lg:text-[3rem]") Check out our
+            span(class="text-[#7863FA]") Client Side 
+            span Portal
+        p(class=" w-auto text-lg font-normal  mt-1 lg:mt-3 ")
+            
+
+    nuxt-img(src="images/demo/freshandtidy.png" format="webp" class="w-[93%] lg:w-3/5 h-full  border-[1px] border-[#000000a1]" )
+
+    div(class="flex flex-col w-[93%] items-center justify-center px-2 lg:px-0")
+        div(class="flex mt-[2rem] lg:mt-[4rem]  w-auto relative flex-col text-3xl lg:text-[3rem] items-center  font-bold")
+            //- nuxt-img(src="images/v2/star.png" width="60px" class=" absolute -top-[2rem] -right-[2rem]")
+            //- nuxt-img(src="images/v2/star1.png" width="50px" class=" absolute top-[0rem] -right-[6rem]")
+            //- nuxt-img(src="images/v2/star.png" width="40px" class=" absolute top-[2rem] -left-[3rem]")
+            p(class="w-auto flex flex-wrap gap-x-3 items-center justify-center text-3xl lg:text-[3rem]") The 
+                span(class="text-[#7863FA]") booking
+                span(class="")  process
+            p(class=" w-auto text-lg font-normal  mt-1 lg:mt-3 text-center") (Every service includes diffrent questions)
+            
+            
+        div(class="flex-col md:flex-row flex gap-x-8 w-full lg:w-3/5 ")
+            nuxt-img(src="images/demo/FTP1X.png" format="webp" class="w-full lg:w-1/2 h-full mt-8 lg:mt-[3rem] border-[1px] border-[#000000a1] lg:rounded-3xl" )
+            nuxt-img(src="images/demo/FTP2X.png" format="webp" class="w-full lg:w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+        div(class="flex-col md:flex-row flex gap-x-8 w-full lg:w-3/5 ")
+            nuxt-img(src="images/demo/FTP3X.png" format="webp" class="w-full lg:w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+            nuxt-img(src="images/demo/FTP4X.png" format="webp" class="w-full lg:w-1/2 h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+            
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss"></style>
---
+++ components/Demo/Partners.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+div(class="flex flex-col w-full mt-[2rem] gap-y-12 items-center text-black  pt-[2rem] pb-[2rem] lg:pb-[9rem] bg-white")
+    div(class="flex  w-auto relative flex-col text-3xl lg:text-[3rem] items-center  font-bold")
+        //- nuxt-img(src="images/v2/star.png" width="60px" class=" absolute -top-[2rem] -right-[2rem]")
+        //- nuxt-img(src="images/v2/star1.png" width="50px" class=" absolute top-[0rem] -right-[6rem]")
+        //- nuxt-img(src="images/v2/star.png" width="40px" class=" absolute top-[2rem] -left-[3rem]")
+        p(class="w-auto flex flex-wrap gap-x-3 items-center justify-center text-3xl lg:text-[3rem]") Check out our
+            span(class="text-[#7863FA]") MVP
+        p(class=" w-auto text-lg font-normal text-center mt-1 lg:mt-3 ") (Contractor's site)
+            
+
+    nuxt-img(src="images/demo/partners/services.png" format="webp" class="w-[93%] lg:w-3/5 h-full  border-[1px] border-[#000000a1]" )
+    p(class=" w-auto text-lg font-normal -mt-9  text-center") (Set your services, prices, etc)
+
+    div(class="flex flex-col w-[93%] items-center justify-center px-2 lg:px-0")
+        div(class="flex mt-[2rem] lg:mt-[4rem]  w-auto relative flex-col text-3xl lg:text-[3rem] items-center  font-bold")
+            //- nuxt-img(src="images/v2/star.png" width="60px" class=" absolute -top-[2rem] -right-[2rem]")
+            //- nuxt-img(src="images/v2/star1.png" width="50px" class=" absolute top-[0rem] -right-[6rem]")
+            //- nuxt-img(src="images/v2/star.png" width="40px" class=" absolute top-[2rem] -left-[3rem]")
+            p(class="w-auto flex flex-wrap gap-x-3 items-center justify-center text-3xl lg:text-[3rem]") Other 
+                span(class="text-[#7863FA]") Funcionalities
+                span(class="")  
+            p(class=" w-auto text-lg font-normal  mt-1 lg:mt-3 text-center")  
+            
+            
+        div(class="flex-col flex gap-x-8 w-full lg:w-4/5 ")
+            nuxt-img(src="images/demo/partners/localizations1.png" format="webp" class="w-full  h-full mt-8 lg:mt-[3rem] border-[1px] border-[#000000a1] lg:rounded-3xl" )
+            p(class=" w-auto text-lg font-normal  text-center") (Choose your working area)
+            nuxt-img(src="images/demo/partners/quotes.png" format="webp" class="w-full  h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+            p(class=" w-auto text-lg font-normal  text-center") (Review and manage quotes)
+        //- div(class="flex-col flex gap-x-8 w-full lg:w-4/5 ")
+        //-     nuxt-img(src="images/demo/FTP3X.png" format="webp" class="w-full h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+        //-     nuxt-img(src="images/demo/FTP4X.png" format="webp" class="w-full  h-full mt-[3rem] border-[1px] border-[#000000a1]  lg:rounded-3xl" )
+    
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss"></style>
