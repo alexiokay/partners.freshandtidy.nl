41c59d485ba40accf85e4655b6474760bd52b2a0
alexispace <papadi166@gmail.com>
2024-11-24 18:25:34 +0100

    apps configuration

--- apps/web-app/nuxt.config.ts
+++ apps/web-app/nuxt.config.ts
@@ -2,8 +2,28 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  extends: [["../layers/base", { install: true }]],
+  extends: [["../../layers/base", { install: true }]],
   future: {
     compatibilityVersion: 4,
+  },
+  runtimeConfig: {
+    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
+    TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+    TWILIO_SHORT_NAME: process.env.TWILIO_SHORT_NAME,
+    TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER,
+
+    public: {
+      test: "test",
+      cwd: process.cwd(),
+      BASE_URL: process.env.BASE_URL,
+      API_TOKEN: process.env.API_TOKEN,
+      API_URL: process.env.API_URL,
+      HOST: process.env.HOST,
+      FETCH_HOST: process.env.FETCH_HOST,
+      gtm_id: process.env.GOOGLE_TAG_MANAGER_ID,
+      gtm_enabled: process.env.GOOGLE_TAG_MANAGER_ENABLED,
+      gtm_debug: process.env.GOOGLE_TAG_MANAGER_DEBUG,
+    },
   },
 });
--- layers/base/nuxt.config.ts
+++ layers/base/nuxt.config.ts
@@ -124,26 +124,5 @@ export default defineNuxtConfig({
     },
   },
 
-  runtimeConfig: {
-    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-    TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
-    TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
-    TWILIO_SHORT_NAME: process.env.TWILIO_SHORT_NAME,
-    TWILIO_PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER,
-
-    public: {
-      test: "test",
-      cwd: process.cwd(),
-      BASE_URL: process.env.BASE_URL,
-      API_TOKEN: process.env.API_TOKEN,
-      API_URL: process.env.API_URL,
-      HOST: process.env.HOST,
-      FETCH_HOST: process.env.FETCH_HOST,
-      gtm_id: process.env.GOOGLE_TAG_MANAGER_ID,
-      gtm_enabled: process.env.GOOGLE_TAG_MANAGER_ENABLED,
-      gtm_debug: process.env.GOOGLE_TAG_MANAGER_DEBUG,
-    },
-  },
-
   compatibilityDate: "2024-11-23",
 });
